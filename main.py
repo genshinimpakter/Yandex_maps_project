85a556244f926762c10dce5817afb9f83f3c1387
Andrew <alekseev2005amg@gmail.com>
2022-02-07 17:51:50 +0300

    Main window

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('app_des.ui', self)  # Загружаем дизайн
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
